febf67561bdd2f7013ff960a682fe9011f839147
FrederickMih <fredtimermih@gmail.com>
2021-01-22 07:48:34 +0100

    add the my_all method

--- enumerable.rb
+++ enumerable.rb
@@ -31,17 +31,29 @@ module Enumerable
     end
     arr
   end
-end
 
-# testing my methods
-[4, 8, 5].my_each do |i|
-  puts i
-end
+  #   my_all method
+  def my_all?
+    b = *self
+    b.my_each do |i|
+      return false if yield(i) == false
+    end
+    true
+  end
 
-[4, 8, 5].my_each_with_index do |i, index|
-  puts "#{i} at index #{index}"
-end
+  # testing my methods
 
-[4, 8, 5].my_select do |i|
-  puts i if i.even?
+  [4, 8, 5].my_each do |i|
+    puts i
+  end
+
+  [4, 8, 5].my_each_with_index do |i, index|
+    puts "#{i} at index #{index}"
+  end
+
+  [4, 8, 5].my_select do |i|
+    puts i if i > 4
+  end
+
+  puts([4, 8, 5].my_all? { |i| i > 2 })
 end
