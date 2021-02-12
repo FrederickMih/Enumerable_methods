a1fd1abd200c76755bfb9b493b19ba0971309eeb
FrederickMih <fredtimermih@gmail.com>
2021-02-12 12:05:37 +0100

    added my_each test for hashes

--- spec/enumerable_spec.rb
+++ spec/enumerable_spec.rb
@@ -1,11 +1,19 @@
 require_relative '../enumerable.rb'
 
 describe Enumerable do
-let(:array) { [1, 2, 3, 4, 5] }
-let(:hs) { {one: 1, two: 2, } }
-end 
+let(:arr) { [1, 2, 3, 4, 5] }
+let(:hs) { [ one: 1, two: 2, three: 3, four: 4, five: 5 ] }
+let(:rg) { (1..5 ) }
 
 describe "#my_each" do
-    it ""
+    it "returns the array of numbers" do
+      expect(arr.my_each { |n| n } ).to eql(arr)
+    end
+
+    it "returns the hash table itself" do 
+      expect(hs.my_each { |k, _v| k }).to eql(hs)
+    end
     
 end
+
+end 
